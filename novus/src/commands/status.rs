e166227eb55c43d3c0c7824491d9d7aeb9f47448
Shadow7258 <pranav.rdoshi@gmail.com>
2021-09-09 08:42:42 +0530

    fix: added status check

--- novus/src/commands/status.rs
+++ novus/src/commands/status.rs
@@ -1,6 +1,27 @@
 use colored::Colorize;
-use utils::handle_error::handle_error_and_exit;
+use utils::classes::package::Package;
+use utils::get_package::get_package;
+use utils::registry::check_installed;
 
-pub async fn status(_flags: Vec<String>, package: &str) {
+pub async fn status(_flags: Vec<String>, package_name: &str) {
+    let package: Package = get_package(package_name).await;
+    let display_name = package.display_name;
+
+    let installed: bool = check_installed(display_name.clone());
+
+    if installed {
+        println!(
+            "{}{}",
+            display_name.bright_green(),
+            " is installed on your system".bright_green()
+        );
+    } else {
+        println!(
+            "{}{}",
+            display_name.bright_red(),
+            " is not installed on your system".bright_red()
+        );
+    }
+
     std::process::exit(0);
 }
