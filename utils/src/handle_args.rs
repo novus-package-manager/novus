8de683eb3830ee9b66c3599b9d7d2f1be82d03ac
Shadow7258 <pranav.rdoshi@gmail.com>
2021-10-15 11:04:56 +0530

    feat: reading aliases

--- utils/src/handle_args.rs
+++ utils/src/handle_args.rs
@@ -1,11 +1,15 @@
+use crate::classes::aliases::Aliases;
 use crate::constants::commands::{
     ALL_COMMANDS, CLEAN_FLAGS as clean_flags, FORCEQUIT_FLAGS as forcequit_flags,
     INFO_FLAGS as info_flags, INSTALL_FLAGS as install_flags, LIST_FLAGS as list_flags,
     QUIT_FLAGS as quit_flags, SEARCH_FLAGS as search_flags, UNINSTALL_FLAGS as uninstall_flags,
 };
 use crate::constants::help_menu::invalid_command;
+use crate::handle_error::handle_error_and_exit;
 use colored::Colorize;
 use difflib::get_close_matches;
+use std::collections::HashMap;
+use std::fs::read_to_string;
 use std::{io::prelude::*, process};
 
 pub fn verify_args(
@@ -17,7 +21,13 @@ pub fn verify_args(
     let mut new_flags: Vec<String> = flags.clone();
     let mut new_packages: Vec<String> = packages.clone();
 
-    if command != "search" && command != "list" && command != "info" && command != "startup" && command != "config" && command != "alias" {
+    if command != "search"
+        && command != "list"
+        && command != "info"
+        && command != "startup"
+        && command != "config"
+        && command != "alias"
+    {
         new_packages = vec![];
         new_flags = vec![];
         for pkg in packages.iter() {
@@ -28,39 +38,72 @@ pub fn verify_args(
                 pkg_name = pkg_split[0];
                 version = pkg_split[1];
             };
-            let revised_package = get_close_matches(pkg_name, package_list.clone(), 1, 0.6);
             if package_list.contains(&pkg_name) {
                 new_packages.push(pkg.to_string().clone());
-            } else if revised_package.len() == 1 {
-                print!(
-                    "Could not find {} package. {} {} instead? (Y/N): ",
-                    pkg_name.bright_magenta(),
-                    command,
-                    revised_package[0].bright_green()
-                );
-                std::io::stdout()
-                    .flush()
-                    .ok()
-                    .expect("Could not flush stdout");
-                let mut string: String = String::new();
-                let _ = std::io::stdin().read_line(&mut string);
-                if string.trim().to_lowercase() == "y" {
-                    let mut new_package = revised_package[0].to_string();
-                    if command == "install" {
-                        new_package = revised_package[0].to_string() + "@" + version;
-                    }
-                    new_packages.push(new_package.to_string());
-                } else {
-                    process::exit(0);
-                }
             } else {
-                println!(
-                    "Package {} doesn't exist yet :(\n\n{} run {} to view all available packages.",
-                    pkg_name.bright_magenta(),
-                    "info:".bright_blue(),
-                    "novus list".bright_magenta()
-                );
-                std::process::exit(1)
+                let appdata = std::env::var("APPDATA").unwrap_or_else(|_| {
+                    handle_error_and_exit("Failed to locate appdata directory".to_string())
+                });
+                let alias_file_loc = format!(r"{}\novus\aliases.json", appdata);
+                let alias_file_path = std::path::Path::new(&alias_file_loc);
+
+                let contents = read_to_string(&alias_file_path).unwrap_or_else(|_| {
+                    handle_error_and_exit("Failed to read alias file".to_string())
+                });
+
+                let json_content =
+                    serde_json::from_str::<Aliases>(&contents).unwrap_or_else(|_| {
+                        handle_error_and_exit("Failed to parse alias file".to_string())
+                    });
+
+                let aliases: HashMap<String, Vec<String>> = json_content.aliases;
+                let mut new_package: String = "null".to_string();
+
+                for (key, val) in aliases.iter() {
+                    if val.contains(&pkg_name.to_string()) {
+                        new_package = key.to_owned();
+                    }
+                }
+
+                if new_package != "null".to_string() {
+                    if command == "install" {
+                        new_package = new_package + "@" + version;
+                    }
+                    new_packages.push(new_package);
+                } else {
+                    let revised_package = get_close_matches(pkg_name, package_list.clone(), 1, 0.6);
+                    if revised_package.len() == 1 {
+                        print!(
+                            "Could not find {} package. {} {} instead? (Y/N): ",
+                            pkg_name.bright_magenta(),
+                            command,
+                            revised_package[0].bright_green()
+                        );
+                        std::io::stdout()
+                            .flush()
+                            .ok()
+                            .expect("Could not flush stdout");
+                        let mut string: String = String::new();
+                        let _ = std::io::stdin().read_line(&mut string);
+                        if string.trim().to_lowercase() == "y" {
+                            let mut new_package = revised_package[0].to_string();
+                            if command == "install" {
+                                new_package = revised_package[0].to_string() + "@" + version;
+                            }
+                            new_packages.push(new_package.to_string());
+                        } else {
+                            process::exit(0);
+                        }
+                    } else {
+                        println!(
+                            "Package {} doesn't exist yet :(\n\n{} run {} to view all available packages.",
+                            pkg_name.bright_magenta(),
+                            "info:".bright_cyan(),
+                            "novus list".bright_magenta()
+                        );
+                        std::process::exit(1)
+                    }
+                }
             }
         }
     }
